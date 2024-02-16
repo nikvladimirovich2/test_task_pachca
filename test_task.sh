f1fa97fc5b52f6793ff3d9007a9f4ffc052b3726
Nikita <92884795+nikvladimirovich2@users.noreply.github.com>
2024-02-16 15:28:38 +0300

    Create test_task.sh

---
+++ test_task.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker build -t ruby_pachca:1.0 ./
+docker-compose up -d && sleep 20
+file=$(docker exec -i maria-master mysql -u root -e 'SHOW MASTER STATUS;' | grep master | awk '{ print $1 }')
+pos=$(docker exec -i maria-master mysql -u root -e 'SHOW MASTER STATUS;' | grep master | awk '{ print $2 }')
+host=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' maria-master)
+docker exec -i maria-slave mysql -u root -e "CHANGE MASTER TO MASTER_HOST='$host', MASTER_USER='root', MASTER_LOG_FILE='$file', MASTER_LOG_POS=$pos;"
+docker exec -i maria-slave mysql -u root -e "START SLAVE;"
+docker exec -i maria-master mysql -u root app < ./structure.sql
+sleep 5 && curl http://localhost:8090
+read -rp "Можно проверить в браузере, что сайт доступен и нажать enter" approve
+docker stop maria-slave && curl http://localhost:8090
+read -rp "Можно проверить в браузере, что сайт доступен и нажать enter" approve
+docker start maria-slave && docker stop maria-master
+sleep 5 && curl http://localhost:8090
+read -rp "Можно проверить в браузере, что сайт доступен и нажать enter" approve
